ce23a4eb3a399efd2b7b18042ade14a6e07c8d2d
Mithun Pradeep <m.dhandapani@walmart.com>
2024-03-11 19:42:21 +0530

    Revert "round 2 changes"
    This reverts commit f48bf699768f6f85cc1489911ca5c9aaccdb30b2.

--- src/components/molecules/UserList.tsx
+++ src/components/molecules/UserList.tsx
@@ -1,5 +1,5 @@
 import Card from "@mui/material/Card";
-import {Button, CardActionArea, TextField} from "@mui/material";
+import {CardActionArea} from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -10,15 +10,10 @@ import {useNavigate} from "react-router-dom";
 type Props = {
     user?: string
     followers?: boolean
-    details?: boolean
 }
-const UserList = ({user, followers, details}: Props) => {
+const UserList = ({user, followers}: Props) => {
 
     const [users, setUsers] = useState<Array<any>>([])
-    const [username, setUsername] = useState("")
-    const [location, setLocation] = useState("")
-    const [queryParam, setQueryParam] = useState("")
-
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -27,63 +22,37 @@ const UserList = ({user, followers, details}: Props) => {
             url = `https://api.github.com/users/${user}/followers`
         } else if (user && !followers) {
             url = `https://api.github.com/users/${user}/following`
-        } else if (queryParam) {
-            url = `https://api.github.com/search/users?${queryParam}`
         } else {
             url = `https://api.github.com/users`
         }
-        fetch(url).then((res) => res.json()).then((res) => {
-            if (queryParam) {
-                setUsers(res['items'])
-            } else {
-                setUsers(res)
-            }
-        }).catch(() => console.log("Error getting Users"))
-    }, [user, followers, queryParam])
+        fetch(url).then((res) => res.json()).then((res) => setUsers(res)).catch(() => console.log("Error getting Users"))
+    }, [user, followers])
 
     const getUserDetails = (user: any) => {
         navigate(`/details/${user.login}`)
     }
 
-    const getUserBySearchCriteria = () => {
-        if (username || location) {
-            setQueryParam(`q=${username}+location:${location}`)
-        } else {
-            setQueryParam("")
-        }
-
-    }
-
     return (
-        <>
-            {! details && <div className="flex flex-row p-3 justify-center m-auto">
-                <TextField id="outlined-basic" label="Username" variant="outlined"
-                           onChange={(e) => setUsername(e.target.value)}/>
-                <TextField id="outlined-basic" label="Location" variant="outlined"
-                           onChange={(e) => setLocation(e.target.value)}/>
-                <Button variant="contained" onClick={getUserBySearchCriteria}>Search</Button>
-            </div>}
-            <div className="flex flex-row justify-center m-auto flex-wrap">
-                {!!users.length && users.map((user: any, index: number) => (
-                    <Card onClick={() => getUserDetails(user)} key={user.login} className="w-[40%] m-3 p-3 box-border">
-                        <CardActionArea className="!flex !flex-row !justify-stretch">
-                            <CardMedia
-                                className="!w-1/4"
-                                component="img"
-                                image={user.avatar_url}
-                                loading="lazy"
-                                alt={user.login}
-                            />
-                            <CardContent className="w-2/3">
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {user.login}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                ))}
-            </div>
-        </>
+        <div className="flex flex-row justify-center m-auto flex-wrap">
+            {!!users.length && users.map((user: any, index: number) => (
+                <Card onClick={() => getUserDetails(user)} key={user.login} className="w-[40%] m-3 p-3 box-border">
+                    <CardActionArea className="!flex !flex-row !justify-stretch">
+                        <CardMedia
+                            className="!w-1/4"
+                            component="img"
+                            image={user.avatar_url}
+                            loading="lazy"
+                            alt={user.login}
+                        />
+                        <CardContent className="w-2/3">
+                            <Typography gutterBottom variant="h5" component="div">
+                                {user.login}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            ))}
+        </div>
     )
 }
 
--- src/components/pages/UserDetails.tsx
+++ src/components/pages/UserDetails.tsx
@@ -21,16 +21,17 @@ type Props = {}
 const UsersDetails = (props: Props) => {
     const {username} = useParams();
     const [userDetails, setUserDetails] = useState<any>(false)
-    const [value, setValue] = useState('1');
+    const [value, setValue] = useState('0');
 
 
     useEffect(() => {
         fetch(`https://api.github.com/users/${username}`).then((res) => res.json()).then(res => setUserDetails(res)).catch(() => console.log("Error getting users details"))
-        setValue('1')
+        setValue('0')
     }, [username])
 
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        console.log(newValue)
         setValue(newValue);
     };
 
@@ -76,9 +77,9 @@ const UsersDetails = (props: Props) => {
                                         </Box>
                                         <TabPanel value="1"><RepoList
                                             username={userDetails.login}></RepoList></TabPanel>
-                                        <TabPanel value="2"><UserList user={userDetails.login} details={true}
+                                        <TabPanel value="2"><UserList user={userDetails.login}
                                                                       followers={true}></UserList></TabPanel>
-                                        <TabPanel value="3"><UserList user={userDetails.login} details={true}
+                                        <TabPanel value="3"><UserList user={userDetails.login}
                                                                       followers={false}></UserList></TabPanel>
                                     </TabContext>
                                 </Box>
